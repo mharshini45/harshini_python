69555506206e576ee5ce3d3feef140127b8ca7c5
mharshini45 <mharshini45@gmail.com>
2026-06-11 21:00:19 +0530

    Add files via upload

---
+++ Intro Task 2.py
@@ -0,0 +1,92 @@
+print("HELLO WORLD")
+
+print("Welcome to the world of Python")
+
+print("This is an 'IMPORTANT' News")
+
+print("Good Book's")
+
+n=int(input("enter the decimal no:"))
+print(hex(n))
+
+#perimeter of squ,rec,tri,circle
+a=int(input("enter the no:"))
+b=int(input("enter the no:"))
+c=int(input("enter the no:"))
+r=2
+print(4*a)
+print(a+b+c)
+print(2*(a+b))
+print(2*3.14*r)
+
+print("Were"+"wolf")
+print("Door"+"man")
+print("4"+"chan")
+print(str(4)+"chan")
+print(4*"test")
+
+a=input("enter the name:")
+b=int(input("enter the age:"))
+n=b-100
+print("after these yrs you will turn into 100:",n)
+
+x=3*4%5
+print(x)
+x=-3+4-7*8/5%10
+print(x)
+x=2
+y=1
+z=3
+test=(4*x+3)*(2*y+2*z-4)
+print(test)
+i=1
+j=2
+m=i*100+j*10
+print(m)
+mult=1
+sub=2
+on=mult*sub/4-6/2+2/3*6
+
+a=int(input("enter the no:"))#12
+b=int(input("enter the no:"))#34
+c=int(input("enter the no:"))#45
+d=a>b and a<c
+e=a>=b or a<=c
+f=a==b or b>c
+g=b>15 and c<10 or a>50
+print(d)
+print(e)
+print(f)
+print(g)
+
+a=int(input("enter the no:"))
+if a>0:
+    print("a is the largest no")
+else:
+    print("a is the smallest no")
+
+a=34.56
+print(type(a))
+
+b=str(input("enter the string"))
+print(type(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
